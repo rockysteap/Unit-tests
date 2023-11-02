bab3e234cdf006b9f6545129a63bf6c761be8cdc
Ilia Mediankin <i.mediankin@gmail.com>
2023-11-02 17:50:36 +0300

    fix(HomeWork01): edit gitignore -> add tests to commit

---
+++ HomeWork01/src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        // Task1
+        CalculatorTest.testWithAssertJ();
+
+        // Task2
+        ShopTest.testWithAssertJ();
+
+    }
+
+}
